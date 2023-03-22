485a39862ddd4cd4b8b497f5c4f397f2c499b1a2
publishername <62213479+PublisherName@users.noreply.github.com>
2023-03-22 13:49:02 +0545

    Helper imported in __init__

--- test.py
+++
@@ -1,9 +0,0 @@
-from nepalithar import Caste
-
-
-caste_obj = Caste()
-print(caste_obj.get(1))
-print(caste_obj.detect("Rajhesh hamal Subash Ghimire"))
-print("घिमिरे",caste_obj.is_caste(" घिमिरे "))
-print(caste_obj.get_position("शुभस घिमिरे Madhu Bhattarai"))
-print(caste_obj.split_name("Rajesh Hamal Madhu Bhattarai"))
